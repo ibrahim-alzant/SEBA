965bf8216f9f3e1f982dfc7d46eef9fead6b20ac
Jimmy Abu Al Denien <jimmy@hotmail.co.il>
2015-06-10 09:49:28 +0200

    - Updated the panel headers to match the theme - Pre-defined new teacher and new course details - username: "teacher1" and password is "password" - Added a picture as sample for teacher profile image - Commented out the 2.sql - Fixed the main screen category search

--- elbaile_latino/conf/evolutions/default/2.sql
+++ elbaile_latino/conf/evolutions/default/2.sql
@@ -1,17 +1,17 @@
-# --- !Ups
-INSERT INTO language (language_name, id) VALUES ('Englich', language_seq.nextval);
-INSERT INTO language (language_name, id) VALUES ('German', language_seq.nextval);
-INSERT INTO language (language_name, id) VALUES ('Arabic', language_seq.nextval);
-INSERT INTO language (language_name, id) VALUES ('Russian', language_seq.nextval);
-
-INSERT INTO gender (gender_name,id) VALUES ('Male', gender_seq.nextval);
-INSERT INTO gender (gender_name,id) VALUES ('Female', gender_seq.nextval);
-
-INSERT INTO dance_style (dance_style_name, id) VALUES ('Tango', dance_style_seq.nextval);
-INSERT INTO dance_style (dance_style_name, id) VALUES ('Samba', dance_style_seq.nextval);
-INSERT INTO dance_style (dance_style_name, id) VALUES ('Salsa', dance_style_seq.nextval);
-
-# --- !Downs
-DELETE FROM language;
-DELETE FROM gender;
-DELETE FROM dance_style;
+-- # --- !Ups
+-- INSERT INTO language (language_name, id) VALUES ('Englich', language_seq.nextval);
+-- INSERT INTO language (language_name, id) VALUES ('German', language_seq.nextval);
+-- INSERT INTO language (language_name, id) VALUES ('Arabic', language_seq.nextval);
+-- INSERT INTO language (language_name, id) VALUES ('Russian', language_seq.nextval);
+--
+-- INSERT INTO gender (gender_name,id) VALUES ('Male', gender_seq.nextval);
+-- INSERT INTO gender (gender_name,id) VALUES ('Female', gender_seq.nextval);
+--
+-- INSERT INTO dance_style (dance_style_name, id) VALUES ('Tango', dance_style_seq.nextval);
+-- INSERT INTO dance_style (dance_style_name, id) VALUES ('Samba', dance_style_seq.nextval);
+-- INSERT INTO dance_style (dance_style_name, id) VALUES ('Salsa', dance_style_seq.nextval);
+--
+-- # --- !Downs
+-- DELETE FROM language;
+-- DELETE FROM gender;
+-- DELETE FROM dance_style;
