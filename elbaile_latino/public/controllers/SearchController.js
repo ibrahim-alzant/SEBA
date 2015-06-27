358dc7efa9f92d230fd1054a7da765794626f8d8
Jimmy Abu Al Denien <jimmy@hotmail.co.il>
2015-06-27 19:14:14 +0200

    - Implemented search courses functionality with persistent criteria

--- elbaile_latino/public/controllers/SearchController.js
+++
@@ -1,9 +0,0 @@
-ElBaileLatino.controller('SearchController', function ($scope, $http) {
-
-    $http({
-        method: 'GET',
-        url: "/language/all",
-    }).success(function (data) {
-        $scope.languages = data;
-    });
-});
