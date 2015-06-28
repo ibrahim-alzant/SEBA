cd7377599407b36a50a4c9c7f196b7b59386d427
BezobidnijPsih <mulnok07@rambler.ru>
2015-06-28 04:16:27 +0300

    - age filter

--- elbaile_latino/public/controllers/PartnerSearchController.js
+++ elbaile_latino/public/controllers/PartnerSearchController.js
@@ -10,6 +10,7 @@ ElBaileLatino.controller('PartnerSearchController', function ($scope, $http) {
     $scope.name = document.getElementById("userName").value;
     $scope.student = null;
     $scope.students = [];
+    $scope.age = "18,45";
 
     $http({
         method: 'GET',
@@ -17,27 +18,31 @@ ElBaileLatino.controller('PartnerSearchController', function ($scope, $http) {
     }).success(function (data) {
         $scope.students = data;
         angular.forEach($scope.students, function (student) {
+            var ageDifMs = Date.now() - new Date(student.dateOfBirth).getTime();
+            var ageDate = new Date(ageDifMs); // miliseconds from epoch
+            student.age = Math.abs(ageDate.getUTCFullYear() - 1970);
             if (student.userName == $scope.name) {
                 $scope.student = student;
-                if(student.gender.id==1){
+                if (student.gender.id == 1) {
                     $scope.gender = 2;
-                }else{
+                } else {
                     $scope.gender = 1;
                 }
             }
         })
     });
-    $scope.selectedStudents = function(){
-        var temp =[];
+    $scope.selectedStudents = function () {
+        var temp = [];
         //TODO don't put user into search results
-        angular.forEach($scope.students, function(student){
-           if(student.gender.id==$scope.gender){
-               temp.push(student);
-           }
+        angular.forEach($scope.students, function (student) {
+            if ((student.gender.id == $scope.gender) &&
+                ( student.age >= parseFloat($scope.age.split(',')[0]))&&
+                ( student.age <=  parseFloat($scope.age.split(',')[1]))) {
+                temp.push(student);
+            }
         });
         return temp;
     };
-
 
 
 });
